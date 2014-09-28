53f9618016832fad3e8c4be5e991348b82f05251
Philipp Te <philipptemminghoff@gmx.de>
2014-09-28 03:06:55 +0200

    bit work for trailer window

--- TrailerWindow.py
+++ TrailerWindow.py
@@ -39,6 +39,7 @@ ACTION_RIGHT = [2]
 ACTION_0 = [58, 18]
 ACTION_PLAY = [79]
 ACTION_SELECT_ITEM = [7]
+LISTS = [8001, 8002, 8003, 8004, 8005, 8006, 8007]
 
 
 class TrailerWindow(xbmcgui.WindowXML):
@@ -54,17 +55,25 @@ class TrailerWindow(xbmcgui.WindowXML):
 
     def onAction(self, action):
         action_id = action.getId()
+        focusedcontrol = self.getFocusId()
         if action_id in ACTION_PREVIOUS_MENU:
             self.close()
         elif action_id in ACTION_SHOW_INFO:
-            focusedcontrol = self.getFocusId()
             MoveProperties(focusedcontrol, focusedcontrol)
             xbmc.executebuiltin("SetProperty(PanelWidgetInfo,true,home))")
             xbmc.executebuiltin("SetProperty(WidgetPosition,Widget1,home)")
             xbmc.executebuiltin("SetProperty(WidgetType,movies,home)")
-            builtin = "RunScript(script.extendedinfo,info=extendedinfo,id=%s,imdbid=%s)" % (xbmc.getInfoLabel("ListItem.Property(ID)"), xbmc.getInfoLabel("ListItem.Property(imdbid)"))
+            builtin = "RunScript(script.extendedinfo,info=extendedinfo,id=%s,imdbid=%s)" % (xbmc.getInfoLabel("Window(home).Property(ID)"), xbmc.getInfoLabel("Window(home).Property(imdbid)"))
             xbmc.executebuiltin(builtin)
             xbmc.executebuiltin("ActivateWindow(1165)")
+        # elif action_id in ACTION_LEFT:
+        #     for controlnumber in LISTS:
+        #         if controlnumber != focusedcontrol:
+        #             xbmc.executebuiltin("Control.Move(%i,-1)" % (controlnumber))
+        # elif action_id in ACTION_RIGHT:
+        #     for controlnumber in LISTS:
+        #         if controlnumber != focusedcontrol:
+        #             xbmc.executebuiltin("Control.Move(%i,1)" % (controlnumber))
 
     def onClick(self, controlId):
         pass
